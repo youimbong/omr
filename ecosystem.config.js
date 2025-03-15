0048df01ec92f6388af829d80fb66ce082a5bbdd
youimbong <you4164@gmail.com>
2025-03-15 15:32:39 +0900

    feat:

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,10 @@ module.exports = {
       instances: 'max', // 또는 원하는 인스턴스 수
       script: '.output/server/index.mjs',
       env: {
-        PORT: 3000,
-        NODE_ENV: 'production',
-        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '1234' // 환경 변수에서 관리자 비밀번호 가져오기
+        PORT: process.env.PORT || 3000,
+        NODE_ENV: process.env.NODE_ENV || 'production',
+        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '1234', // 환경 변수에서 관리자 비밀번호 가져오기
+        DATA_PATH: process.env.DATA_PATH || './data' // 데이터 저장 경로
       },
       watch: false,
       max_memory_restart: '1G',
