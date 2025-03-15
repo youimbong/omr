29b2aedce1560561c8e4fe55732b287c3db79392
youimbong <you4164@gmail.com>
2025-03-15 15:42:15 +0900

    feat:

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,20 @@
-module.exports =  {
+require('dotenv').config();
+
+module.exports = {
   apps: [
     {
       name: 'omr-app',
       exec_mode: 'cluster',
-      instances: 1, // 또는 원하는 인스턴스 수
-      script: '.output/server/index.mjs',
+      instances: 1,
+      script: './.output/server/index.mjs',
       env: {
-        PORT: process.env.PORT || 3000,
+        PORT: process.env.PORT || 3001,
         NODE_ENV: process.env.NODE_ENV || 'production',
-        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '1234', // 환경 변수에서 관리자 비밀번호 가져오기
-        DATA_PATH: process.env.DATA_PATH || './data' // 데이터 저장 경로
+        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '1234',
+        DATA_PATH: process.env.DATA_PATH || './data'
       },
       watch: false,
+      max_memory_restart: '1G',
       merge_logs: true,
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       error_file: './logs/error.log',
